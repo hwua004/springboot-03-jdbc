605f8235e624f27b5acad9b54338d81da3736481
tom_g <tomcat_g@163.com>
2018-11-27 11:00:39 +0800

    springboot整合jdbc

--- src/main/java/com/hwua/springboot/entity/User.java
+++ src/main/java/com/hwua/springboot/entity/User.java
@@ -1,7 +1,14 @@
 package com.hwua.springboot.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-
+@NoArgsConstructor
+@Data
+@ToString
+@AllArgsConstructor
 public class User {
 
     private Integer id;
@@ -12,85 +19,5 @@ public class User {
     private String emial;
     private String status;
 
-    public User() {
-    }
 
-    public User(Integer id, String username, String password, String realname, String phone, String emial, String status) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.realname = realname;
-        this.phone = phone;
-        this.emial = emial;
-        this.status = status;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRealname() {
-        return realname;
-    }
-
-    public void setRealname(String realname) {
-        this.realname = realname;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmial() {
-        return emial;
-    }
-
-    public void setEmial(String emial) {
-        this.emial = emial;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", realname='" + realname + '\'' +
-                ", phone='" + phone + '\'' +
-                ", emial='" + emial + '\'' +
-                ", status='" + status + '\'' +
-                '}';
-    }
 }
